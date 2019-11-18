ce71b46a2ae5a83360401611d9b890d9cf14e9b2
Duong Dang <dddangmedia@gmail.com>
2019-11-18 09:37:27 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School
+  attr_reader :roster
+  
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+  
+  def add_student(student_name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << student_name
+  end
+  
+  def grade(grade)
+    @roster[grade].each{|student| puts student}
+  end
+  
+  def sort 
+  nu_hash = {}
+  roster.each do |x, y| 
+      nu_hash[x] = y.sort 
+    end 
+    nu_hash
+  end
+end
